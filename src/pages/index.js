2568f0511046c9cad9187589c06c1cd55402e1c5
iEXCEL-UNMC <85242160+iEXCEL-UNMC@users.noreply.github.com>
2022-05-18 13:46:08 -0500

    dsdsds

--- src/pages/index.js
+++ src/pages/index.js
@@ -83,7 +83,7 @@ const IndexPage = () => (
   <Layout>
     <Header />
 
-    <header className="masthead">
+    {/* <header className="masthead">
       <div className="container h-100">
         <div className="row h-100 align-items-center justify-content-center text-center">
           <div className="col-lg-10 align-self-end">
@@ -107,9 +107,39 @@ const IndexPage = () => (
           </div>
         </div>
       </div>
-    </header>
+    </header> */}
 
-    <section className="page-section bg-primary" id="about">
+
+<div class="section">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-2">
+          <div class="d-flex flex-column  p-3 justify-content-center align-items-center align-self-center"> <a class="navbar-brand" href="#">
+              <b> BRAND</b>
+            </a> <button class="navbar-toggler navbar-toggler-right border-0" type="button" data-toggle="collapse" data-target="#navbar11" style="">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse d-flex flex-column" id="navbar11">
+              <ul class="navbar-nav flex-column">
+                <li class="nav-item"> <a class="nav-link" href="#">Features</a> </li>
+                <li class="nav-item"> <a class="nav-link" href="#">Pricing</a> </li>
+                <li class="nav-item"> <a class="nav-link" href="#">Product</a> </li>
+              </ul>
+              <ul class="navbar-nav flex-column">
+                <li class="nav-item"> <a class="nav-link" href="#">Services</a> </li>
+                <li class="nav-item"> <a class="nav-link" href="#">About</a> </li>
+                <li class="nav-item"> <a class="nav-link" href="#">FAQ</a> </li>
+              </ul> <a class="btn navbar-btn  btn-secondary text-light">Contact us</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6"></div>
+        <div class="col-md-4"></div>
+      </div>
+    </div>
+  </div>
+
+    {/* <section className="page-section bg-primary" id="about">
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-lg-8 text-center">
@@ -131,9 +161,9 @@ const IndexPage = () => (
           </div>
         </div>
       </div>
-    </section>
+    </section> */}
 
-    <section className="page-section" id="services">
+    {/* <section className="page-section" id="services">
       <div className="container">
         <h2 className="text-center mt-0">At Your Service</h2>
         <hr className="divider my-4" />
@@ -176,11 +206,11 @@ const IndexPage = () => (
           </div>
         </div>
       </div>
-    </section>
+    </section> */}
 
-    <section id="portfolio">
+    {/* <section id="portfolio">
       <Gallery images={img_set} />
-    </section>
+    </section> */}
 
     <section className="page-section bg-dark text-white">
       <div className="container text-center">
